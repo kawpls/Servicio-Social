827b43cec097ecc57d1822b73954d87b40e66f53
JOSE MANUEL UGALDE BORBOLLA <jmugaldeb@gmail.com>
2023-03-09 08:11:00 -0600

    Update

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,14 @@
 module.exports = {
-
   content: ["./**/*.html"],
-
   theme: {
+    container: {
+      center: true,
+    },
     extend: {
       colors: {},
     },
   },
-
+  variants: {},
   plugins: [require("@tailwindcss/typography"),
   require("daisyui"),
   ],
